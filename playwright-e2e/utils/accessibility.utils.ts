91c03f22e80ae86da9f7b2e24d255e1151c7149f
Oliver Scott <oliver.scott@justice.gov.uk>
2025-12-05 09:58:48 +0000

    Remove redundant file

--- playwright-e2e/utils/accessibility.utils.ts
+++
@@ -1,9 +0,0 @@
-import axe from 'axe-core';
-
-export async function runAxeAuditIgnoringRules(page, ignoreRules: string[] = []) {
-  const axePath = require.resolve('axe-core/axe.min.js');
-  await page.addScriptTag({ path: axePath });
-
-  const results = await page.evaluate(async () => axe.run());
-  return results.violations.filter((v) => !ignoreRules.includes(v.id));
-}
