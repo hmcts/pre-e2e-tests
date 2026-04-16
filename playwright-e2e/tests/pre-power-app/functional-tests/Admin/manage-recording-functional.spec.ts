6fcb2929e3414bbf79b0fcdef89ee4de65cbc45c
Komal <97675424+komaldabhi1509@users.noreply.github.com>
2026-04-16 11:42:01 +0100

    updated manage recording tes

--- playwright-e2e/tests/pre-power-app/functional-tests/Admin/manage-recording-functional.spec.ts
+++ playwright-e2e/tests/pre-power-app/functional-tests/Admin/manage-recording-functional.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '../../../../fixtures';
+import { expect, test } from '../../../../fixtures';
 import { config } from '../../../../utils';
 
 test.describe('Set of tests to verify functionality of manage recordings page for pre-level 1 user', () => {
@@ -20,9 +20,10 @@ test.describe('Set of tests to verify functionality of manage recordings page fo
       await test.step('Pre-requisite step click on manage recordings button', async () => {
         await powerApp_ManageRecordingsPage.$interactive.manageRecordingsButton.click();
         await powerApp_ManageRecordingsPage.verifyUserIsOnManageRecordingsPage();
-        await apiClient.createCase(1, 1);
       });
-      await test.step('Verify user is able to view the recording details on manage recordings page', async () => {});
+      await test.step('Verify user is able to view the recording details on manage recordings page', async () => {
+        await expect(powerApp_ManageRecordingsPage.$interactive.manageRecordingsLabel).toBeVisible({ timeout: 15000 });
+      });
     },
   );
 });
